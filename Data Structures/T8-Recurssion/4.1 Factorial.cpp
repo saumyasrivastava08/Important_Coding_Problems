389e749171b29eaaf0679d231f01f99a223e6e26
Gaurav Bhanot <71587244+gb07bh@users.noreply.github.com>
2021-10-01 00:08:34 +0530

    Factorial.cpp

--- Data Structures/T8-Recurssion/4.1 Factorial.cpp
+++ Data Structures/T8-Recurssion/4.1 Factorial.cpp
@@ -1,3 +1,13 @@
+// Test Cases
+// Case 1:
+//(input)	Enter the number 5	
+//(output)	The factorial is 120
+
+// Case 2:
+//(input)	Enter the number 10	
+//(output)	The factorial is 3628800
+
+// Code
 #include <iostream>
 using namespace std;
 
@@ -12,8 +22,10 @@ int fact( int n)
 int main()
 {   
 	int num;
+	
     cout << "Enter the number: " << "\n";
     cin >> num;
     cout << " The factorial is " << fact(num) << " \n ";
+	
     return 0;
 }
