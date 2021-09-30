ed5d6e86b4a404742327efc606045c817eb0f24f
Aastha Rastogi <54110949+astelrastogi@users.noreply.github.com>
2021-09-30 16:48:22 +0530

    Update 1.10 Check Occurrences.cpp

--- Data Structures/T8-Recurssion/1.10 Check Occurrences.cpp
+++ Data Structures/T8-Recurssion/1.10 Check Occurrences.cpp
@@ -1,2 +1,20 @@
+//Program to find all occurrences of a number(key) in an array
 #include<iostream>
 using namespace std;
+void allOccurrences(int i, int *a, int n, int key){
+  if(i==n){
+    return;
+  }
+  if(a[i]==key){
+    cout<<key<<" found at index: "<<i<<endl;
+  }
+  allOccurrences(i+1,a,n,key);
+}
+int main(){
+  int a[]={7,8,-2,3,-2,5,-2,4,7};
+  int n=sizeof(a)/sizeof(int);
+  int key = -2;
+  allOccurrences(0, a, n, key);
+}
+  
+  
