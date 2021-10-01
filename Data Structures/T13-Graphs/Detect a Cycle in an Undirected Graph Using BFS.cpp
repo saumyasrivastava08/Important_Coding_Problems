94c144f8e6540b288b3565ae4810e29bf9218400
Akshat Saxena <76419575+tom635@users.noreply.github.com>
2021-10-01 09:54:15 +0530

    Create Detect a Cycle in an Undirected Graph Using BFS.cpp

---
+++ Data Structures/T13-Graphs/Detect a Cycle in an Undirected Graph Using BFS.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <numeric>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <list>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <numeric>
+#include <iterator>
+#include <bitset> 
+#include <cstring>
+using namespace std;
+class Solution {
+// Detect a Cycle in an Undirected Graph Using BFS in Solution class
+public:
+    bool checkForCycle(int s, int V, vector<int> adj[], vector<int>& visited)
+    {
+        vector<int> parent(V, -1);
+     
+        // Create a queue for BFS
+        queue<pair<int,int> > q;
+     
+        visited[s] = true;
+        q.push(make_pair(s, -1));
+      //where -1 represent that there is no parent of this node;
+        while (!q.empty()) {
+     
+            int node = q.front().first;
+            int par = q.front().second;
+            q.pop();
+     
+            for (auto it : adj[node]) {
+                if (!visited[it]) {
+                    visited[it] = true;
+                    q.push(make_pair(it, node));
+                }
+                else if (par != it)
+                    return true;
+            }
+        }
+        return false;
+    }
+public:
+	bool isCycle(int V, vector<int>adj[]){
+	    vector<int> vis(V, 0); 
+	    for(int i = 0;i<V;i++) {
+            // Checking for different components of the graph
+	        if(!vis[i]) {
+	            if(checkForCycle(i, V, adj, vis)) return true; 
+	        }
+	    }
+	    
+	    return false; 
+	}
+};
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		bool ans = obj.isCycle(V, adj);
+		if(ans)
+			cout << "1\n";
+		else cout << "0\n";
+	}
+	return 0;
+} 
